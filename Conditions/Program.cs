a10177b48b583536f8f70704a109f01db63404ff
poojagoyal27 <pgoyal27@mail.fresnostate.edu>
2019-01-11 21:50:21 -0800

    if else

--- Conditions/Program.cs
+++ Conditions/Program.cs
@@ -6,7 +6,26 @@ namespace Conditions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Choose Lucky Number");
+            Console.Write("Press 1 , 2, or 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = "";
+
+            if (userValue == "1")
+                message = "you won a new car";
+            else if (userValue == "2")
+                message = "you won new cat";
+            else if (userValue == "3")
+                message = "you won a hat";
+            else
+            {
+                message = "invalid input";
+            }
+
+            Console.WriteLine(message);
+            Console.ReadLine();
         }
+       
     }
 }
