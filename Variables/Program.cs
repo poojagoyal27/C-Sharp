047a3b1a564245ca93c22da718ff5122a5691b8d
poojagoyal27 <pgoyal27@mail.fresnostate.edu>
2019-01-11 20:57:29 -0800

    Variables int, string

---
+++ Variables/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             int y;
+           int  x = 5;
+             y = x + 7;
+             Console.WriteLine(y);
+             Console.ReadLine();
+             */
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name:");
+            string myFirstname;
+            myFirstname = Console.ReadLine();
+
+            string myLastname;
+            Console.Write("Type your last name:");
+            myLastname = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstname + " " + myLastname);
+            Console.ReadLine();
+         }
+
+    }
+}
