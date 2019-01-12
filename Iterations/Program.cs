a613eec0ab2991c99f6c0fbaadbe3d38627143b5
poojagoyal27 <pgoyal27@mail.fresnostate.edu>
2019-01-11 22:49:10 -0800

    foreach, for loop and arrays

---
+++ Iterations/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Iterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // int[] numbers = new int[5];
+           string[] names = new string[] {"pooja" , "blackAss" , "fatty" , "skinny"};
+         /*   for (int i =0; i<=names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.ReadLine();*/
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
