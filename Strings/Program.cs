8c26403ee9827dae4cfda098a1027ddd3dc042ee
poojagoyal27 <pgoyal27@mail.fresnostate.edu>
2019-01-12 02:48:01 -0800

    String Manipulation and properties

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Strings
 {
@@ -6,7 +7,40 @@ namespace Strings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //  string myString = "My \"So-Called\" life";
+
+            //     string myString = "My \n life";
+            //     string myString = "go to ypur c:\\ drive";
+
+            //    string myString = @"go to ypur c:\ drive";
+            //  string myString = String.Format("{0}= {1}","First", "Second");
+
+            //   string myString = String.Format("{0:C}",123.45 );
+            //   string myString = String.Format("{0:N}", 12345);
+            //   string myString = String.Format("{0:P}", .123);
+            // string myString = String.Format("{0:(###) ###-####}, 1234567890);
+
+            //string myString = " That Summer we took  ";
+            //  myString = myString.Substring(6, 14);
+
+
+
+            //      myString = String.Format("Length before: {0} ---- length after{1}",  myString.Length,myString.Trim().Length);
+            /*  string myString = "";
+                for(int i =0; i < 100; i++)
+              {
+                  myString += "--" + i.ToString();
+              }*/
+
+            StringBuilder myString = new StringBuilder();
+            for(int i =0; i < 100; i++)
+            {
+                myString.Append("--");
+                myString.Append(i);
+            }
+
+            Console.WriteLine(myString);
+            Console.ReadLine();
         }
     }
 }
