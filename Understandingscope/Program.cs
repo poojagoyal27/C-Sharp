0384d4c62606474225e2fdfe0c18ea2e8cca8e52
poojagoyal27 <pgoyal27@mail.fresnostae.edu>
2019-01-13 16:36:27 -0800

    Scope
    * Understandingscope.sln:
    * Program.cs:
    * Understandingscope.csproj:

---
+++ Understandingscope/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Understandingscope
+{
+    class Program
+    {
+        private static string k = "";
+        static void Main(string[] args)
+        {
+            string j = "";
+            for (int i = 0; i < 10; i++)
+            {
+                j = i.ToString();
+                k = i.ToString();
+                Console.WriteLine(i);
+                if(i == 9)
+                {
+                    string l = i.ToString();
+                }
+                Console.WriteLine(l);
+            }
+            Console.WriteLine(j);
+            Console.WriteLine(k);
+            helperMethod();
+            Console.ReadLine();
+        }
+        static void helperMethod()
+        {
+            Console.WriteLine(k);
+        }
+    }
+}
